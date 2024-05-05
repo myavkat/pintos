138b54ace3423a4f57cb890fba91fa97e2981d5f
Mehmet Yavuz Katrancı <77459135+myavkat@users.noreply.github.com>
2024-05-05 16:10:05 +0000

    basic pointer checking

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,7 +7,11 @@
 #include <kernel/console.h>
 #include "devices/shutdown.h"
 #include "userprog/process.h"
+#include "threads/vaddr.h"
 
+static int get_user (const uint8_t *uaddr) ;
+static bool put_user (uint8_t *udst, uint8_t byte) ;
+static bool is_valid_ptr(const uint8_t *uaddr, int size);
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -32,21 +36,75 @@ syscall_handler (struct intr_frame *f)
       break;
       
     case SYS_EXIT: 
-      exit_status = *(char *)f->esp; // Read exit status from esp (stack)
+      if(!is_valid_ptr(f->esp, 8)){
+        thread_exit(-1);
+        return;
+      }
+      exit_status = *(int *)f->esp + 4; // Read exit status from esp (stack)
 
       process_exit(exit_status); // Terminates the current process
       break;
     
     case SYS_WRITE: 
-      fd = *(int*)((intptr_t)(f->esp) + 4);
-      buffer = *(char**)((intptr_t)(f->esp) + 8); // Read buffer pointer
-      size = *(int*)((intptr_t)(f->esp) + 12); // Read number of bytes
+      if(!is_valid_ptr(f->esp, 16)){
+        thread_exit(-1);
+        return;
+      }
+      
+      fd = *(int*)((char *)(f->esp) + 4);
+      buffer = *(char**)((char *)(f->esp) + 8); // Read buffer pointer
+      size = *(int*)((char *)(f->esp) + 12); // Read number of bytes
       if(fd == 1)
       {
         putbuf(buffer, size); // Write data to the console
       }
       break;
-    
+    case SYS_EXEC:
+      break;
     //othersss
   }
+  
+}
+
+
+/* Reads a byte at user virtual address UADDR.
+   UADDR must be below PHYS_BASE.
+   Returns the byte value if successful, -1 if a segfault
+   occurred. */
+static int
+get_user (const uint8_t *uaddr)
+{
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+       : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+/* Writes BYTE to user address UDST.
+   UDST must be below PHYS_BASE.
+   Returns true if successful, false if a segfault occurred. */
+static bool
+put_user (uint8_t *udst, uint8_t byte)
+{
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+       : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
+}
+
+static bool
+is_valid_ptr(const uint8_t *uaddr, int size){
+  if(uaddr==NULL){
+    return false;
+  }
+  if(!is_user_vaddr(uaddr) || !is_user_vaddr(uaddr + size)){
+    return false;
+  }
+  for (int i = 0; i < size; i++)
+  {
+    if(get_user(uaddr+i)==-1){
+      return false;
+    }
+  }
+  return true;
 }
